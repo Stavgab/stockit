48ba8ed2467265eb09636ef2933c591718c6fd74
Noam Dadush <noamd7755@gmail.com>
2022-07-24 20:35:53 +0300

    added CRUD routes for stocks.controller.ts {/stock, GET} - List of stocks {/stock, POST} - Create stock {/stock/:id, GET} - Stock details {/stock/:id/delete, POST} - Delete a stock {/stock/:id/update, POST} - Update a stock+

--- Server/src/stocks/stocks.dal.ts
+++ Server/src/stocks/stocks.dal.ts
@@ -1,4 +1,4 @@
-import { Collection } from 'mongodb';
+import { Collection, ObjectId } from 'mongodb';
 import { Injectable } from '@nestjs/common';
 import { MongoConnector } from '../mongo/connector';
 import { StockDto } from './dto/stock.dto';
@@ -19,6 +19,24 @@ export class StocksDal {
   public async createStock(stock: StockDto): Promise<void> {
     const collection = await this.getStocksDbConnection();
     await collection.insertOne(stock);
+  }
+
+  public async getStockById(id: ObjectId): Promise<StockDto> {
+    const collection = await this.getStocksDbConnection();
+    const query = { _id: id };
+    return collection.findOne(query);
+  }
+
+  public async deleteStockById(id: ObjectId): Promise<void> {
+    const collection = await this.getStocksDbConnection();
+    const query = { _id: id };
+    await collection.deleteOne(query);
+  }
+
+  public async updateStockById(id: ObjectId, stock: StockDto): Promise<void> {
+    const collection = await this.getStocksDbConnection();
+    const query = { _id: id };
+    await collection.findOneAndUpdate(query, { $set: stock });
   }
 
   private async getStocksDbConnection(): Promise<StocksCollection> {
--- Server/src/stocks/stocks.service.ts
+++ Server/src/stocks/stocks.service.ts
@@ -3,6 +3,7 @@ import { HttpService } from '@nestjs/axios';
 import { ConfigService } from '@nestjs/config';
 import { StocksDal } from './stocks.dal';
 import { StockDto } from './dto/stock.dto';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class StocksService {
@@ -18,5 +19,20 @@ export class StocksService {
 
   public async createStock(stock: StockDto): Promise<void> {
     await this.stocksDal.createStock(stock);
+  }
+
+  public async getStockById(id: ObjectId): Promise<StockDto> {
+    id = new ObjectId(id);
+    return await this.stocksDal.getStockById(id);
+  }
+
+  public async deleteStockById(id: ObjectId): Promise<void> {
+    id = new ObjectId(id);
+    await this.stocksDal.deleteStockById(id);
+  }
+
+  public async updateStockById(id: ObjectId, stock: StockDto): Promise<void> {
+    id = new ObjectId(id);
+    await this.stocksDal.updateStockById(id, stock);
   }
 }
