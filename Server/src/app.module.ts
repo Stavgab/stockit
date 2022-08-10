f6f49967a2878d7321130a15f374c235ef9d6dc8
Noam Dadush <noamd7755@gmail.com>
2022-08-11 00:14:35 +0300

    added basic webscraping features

--- Server/src/app.module.ts
+++ Server/src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { StocksModule } from './stocks/stocks.module';
 import { StockNewsModule } from './stockNews/stockNews.module';
+import { PuppeteerModule } from 'nest-puppeteer';
 
 @Module({
   imports: [
     StocksModule,
     StockNewsModule,
+    PuppeteerModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal: true,
