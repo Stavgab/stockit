b9a00aa685f0dd6e92ddc8e644f34f2559958160
Noam Dadush <noamd7755@gmail.com>
2022-08-15 23:21:59 +0300

    added single stock scrap

--- Server/src/stocks/stocks.service.ts
+++ Server/src/stocks/stocks.service.ts
@@ -6,6 +6,8 @@ import { StockDto } from './dto/stock.dto';
 import { ObjectId } from 'mongodb';
 import { BrowserContext } from 'puppeteer';
 import { InjectContext } from 'nest-puppeteer';
+import { randomInt } from 'crypto';
+import * as fs from 'fs';
 
 @Injectable()
 export class StocksService {
@@ -78,11 +80,68 @@ export class StocksService {
       .replace('[{', '')
       .replace('}]', '')
       .split('},{');
-    for (const stock of data) {
+    for (let i = 0; i < 10; i++) {
       await this.createStock(
-        await this.mapScrapToStock(JSON.parse('{' + stock + '}')),
+        await this.mapScrapToStock(
+          JSON.parse('{' + data[randomInt(data.length) - 1] + '}'),
+        ),
       );
     }
+  }
+
+  public async scrapStockByTicker(ticker: string): Promise<StockDto> {
+    const page = await this.browser.newPage();
+    await page.goto('https://finance.yahoo.com/quote/' + ticker);
+    const stockName = (
+      await page.$$eval(
+        'div > #quote-header-info > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h1',
+        (test) => test.map((t) => t.textContent),
+      )
+    )[0]
+      .split('(')[0]
+      .trim();
+    const marketCap = (
+      await page.$$eval(
+        'div > #quote-summary > div:nth-child(2) > table > tbody > tr:nth-child(1) > td:nth-child(2)',
+        (test) => test.map((t) => t.textContent),
+      )
+    )[0];
+    const stockPrice = (
+      await page.$$eval(
+        'div > #quote-header-info > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > fin-streamer:nth-child(1)',
+        (test) => test.map((t) => t.textContent),
+      )
+    )[0];
+    let marketCapValue;
+    switch (marketCap.charAt(marketCap.length - 1)) {
+      case 'T': {
+        marketCapValue = Number(marketCap.replace('T', '')) * 1000000000000;
+        break;
+      }
+      case 'B': {
+        marketCapValue = Number(marketCap.replace('B', '')) * 1000000000;
+        break;
+      }
+      case 'M': {
+        marketCapValue = Number(marketCap.replace('M', '')) * 1000000;
+        break;
+      }
+    }
+    return {
+      _id: undefined,
+      company: stockName,
+      location: '',
+      marketCap: marketCapValue,
+      price: Number(stockPrice),
+      sector: undefined,
+      stockHistory: undefined,
+      stockNews: [],
+      ticker: ticker,
+    };
+  }
+
+  private async yfinancemap(scrap: string): Promise<void> {
+    fs.writeFileSync('../test.txt', scrap);
   }
 
   private async mapScrapToStock(JSON): Promise<StockDto> {
