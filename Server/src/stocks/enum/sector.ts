0f7044e98825a63384332f618121e5073edafbd0
Noam Dadush <noamd7755@gmail.com>
2022-08-18 22:32:32 +0300

    scrap single stock and create in db

--- Server/src/stocks/enum/sector.ts
+++ Server/src/stocks/enum/sector.ts
@@ -17,4 +17,13 @@ export enum Sector {
   TRANSPORTATION = 'Transportation',
   UNCLASSIFIED = 'Unclassified',
   UTILITIES = 'Utilities',
+  TECHNOLOGY = 'Technology',
+  CONSUMER_CYCLICAL = 'Consumer Cyclical',
+  FINANCIAL_SERVICES = 'Financial Services',
+  REAL_ESTATE = 'Real Estate',
+  CONSUMER_DEFENSIVE = 'Consumer Defensive',
+  HEALTHCARE = 'Healthcare',
+  COMMUNICATION_SERVICES = 'Communication Services',
+  ENERGY = 'Energy',
+  INDUSTRIALS = 'Industrials',
 }
