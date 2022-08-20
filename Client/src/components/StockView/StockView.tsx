c47aff8bcea5bdb1997e125fb06d9a6a1dc5d373
Stav Gabay <stavgab@cs.colman.ac.il>
2022-08-20 20:23:00 +0300

    fix marketCap format

--- Client/src/components/StockView/StockView.tsx
+++ Client/src/components/StockView/StockView.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useEffect, useState } from "react";
-import { Sector } from "../../common/enum/SectorType";
+// import { Sector } from "../../common/enum/SectorType";
 import { StockType } from "../../common/enum/StockType";
 import { CommonCenteredContainer } from "../../common/styles";
 import { DetailsGrid, DetailContainer, Title, Detail } from "./styles";
@@ -23,11 +23,23 @@ const StockView: FC<StockType> = ({
       <DetailsGrid>
         {stock &&
           Object.keys(stock).map((key: string, index) => {
+            if (key === "marketCap")
+              if (stock[key] / 1000000000000 > 1) stock[key] /= 1000000000000;
+              else if (stock[key] / 1000000000 > 1) stock[key] /= 1000000000;
+              else if (stock[key] / 1000000 > 1) stock[key] /= 1000000;
             return (
               <DetailContainer key={index}>
                 <Title>{key}</Title>
                 <Detail>
-                  {key === "sector" ? sector : stock[key as keyof StockType]}
+                  {key === "sector"
+                    ? sector
+                    : key === "marketCap" && stock[key] / 1000000000000 > 1
+                    ? `${stock[key as keyof StockType]}T`
+                    : key === "marketCap" && stock[key] / 1000000000 > 1
+                    ? `${stock[key as keyof StockType]}B`
+                    : key === "marketCap" && stock[key] / 1000000 > 1
+                    ? `${stock[key as keyof StockType]}M`
+                    : stock[key as keyof StockType]}
                 </Detail>
               </DetailContainer>
             );
