204eeb4f05c142ab29c251c15e76bba70caf57a5
Stav Gabay <stavgab@cs.colman.ac.il>
2022-08-28 04:18:47 +0300

    fix StockNewsDelete page

--- Client/src/components/StockNewsDelete/StockNewsDelete.tsx
+++ Client/src/components/StockNewsDelete/StockNewsDelete.tsx
@@ -1,56 +1,98 @@
 import { Button } from "@mui/material";
 import React, { FC, useEffect, useState } from "react";
-import { BodyContainer, Container, TitleContainer } from "./styles";
 import { useNavigate, useParams } from "react-router-dom";
 import { NEWS_ROUTE, SERVER_URL } from "../../utils/Consts";
 import axios from "axios";
+import {
+  ButtonsContainer,
+  CancelButton,
+  DeleteButton,
+  DeleteStockFormContainer,
+  Label,
+  PropContainer,
+  StockDetailsContainer,
+  Text,
+  Title,
+  TrashContainer,
+} from "./styles";
+import { BsTrash } from "react-icons/bs";
+import { ErrorMessage, ErrorMessageContainer } from "../CreateStockForm/styles";
+import { StockNewsType } from "../../common/enum/StockNewsType";
 
 const StockNewsDelete: FC = () => {
-  const [stockNews, setStockNews] = useState({
-    title: "",
-    stockName: "",
-    sectors: "",
-    description: "",
-    author: "",
-    source: "",
-    date: "",
-    stocks: [""],
-  });
+  // const [stockNews, setStockNews] = useState({
+  //   title: "",
+  //   stockName: "",
+  //   sectors: "",
+  //   description: "",
+  //   author: "",
+  //   source: "",
+  //   date: "",
+  //   stocks: [""],
+  // });
+
+  const navigate = useNavigate();
+  const [stockNews, setStockNews] = useState<StockNewsType>();
+  const [isDeletedSuccessfully, setIsDeletedSuccessfully] = useState(false);
   useEffect(() => {
     axios.get(`${SERVER_URL}${NEWS_ROUTE}details/${id}`).then((response) => {
       setStockNews(response.data);
     });
   }, []);
   const { id } = useParams();
-  const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
+
+  const onDelete = () => {
     axios
       .delete(`${SERVER_URL}${NEWS_ROUTE}delete/${id}`)
       .then((res) => {
-        navigate("/");
+        setIsDeletedSuccessfully(true);
+
+        setTimeout(() => navigate("/"), 2000);
       })
       .catch((err) => {
-        console.log(err);
+        setIsDeletedSuccessfully(false);
       });
   };
 
-  const navigate = useNavigate();
-  return (
-    <>
-      <Container>
-        <TitleContainer>Would you like to delete that news?</TitleContainer>
-        <BodyContainer>
-          <p>{stockNews.title}</p>
-          <p>{stockNews.stockName}</p>
-          <p>{stockNews.sectors}</p>
-          <p>{stockNews.author}</p>
-          <p>{stockNews.source}</p>
-          <p>{stockNews.date}</p>
-        </BodyContainer>
-        <Button onClick={handleSubmit}>YES</Button>
-        <Button onClick={() => navigate("/")}>NO</Button>
-      </Container>
-    </>
+  const onCancel = () => {
+    navigate(-1);
+  };
+  return isDeletedSuccessfully ? (
+    <ErrorMessageContainer>
+      <ErrorMessage>The stock has been successfully deleted!</ErrorMessage>
+      <ErrorMessage>please wait while you are redirected</ErrorMessage>
+    </ErrorMessageContainer>
+  ) : (
+    <DeleteStockFormContainer>
+      <TrashContainer>
+        <BsTrash size={50} color="red" />
+      </TrashContainer>
+      <Title>You are going to delete the following Article:</Title>
+      <StockDetailsContainer>
+        {stockNews && (
+          <>
+            <PropContainer>
+              <Label>Title:</Label>
+              <Text>{stockNews.title}</Text>
+            </PropContainer>
+            <PropContainer>
+              <Label>Sector:</Label>
+              <Text>{stockNews.sectors}</Text>
+            </PropContainer>
+            <PropContainer>
+              <Label>Author:</Label>
+              <Text>{stockNews.author}</Text>
+            </PropContainer>
+          </>
+        )}
+      </StockDetailsContainer>
+      <Text>This will delete the Article permanently</Text>
+      <Text>Are you sure?</Text>
+      <ButtonsContainer>
+        <CancelButton onClick={onCancel}>Cancel</CancelButton>
+        <DeleteButton onClick={onDelete}>Delete</DeleteButton>
+      </ButtonsContainer>
+    </DeleteStockFormContainer>
   );
 };
 
